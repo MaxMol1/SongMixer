04d806fdb9bd1732c33748c3e6b9584b6ce73155
MaxMol1 <mmolchanov@ucdavis.edu>
2023-11-30 18:51:18 -0800

    Create skeleton for graph traversal algorithm

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import argparse
+from ast import Dict, List, Tuple
+from lib2to3.pytree import Node
 
 from SongKeyGraphBuilder import SongKeyGraphBuilder
 
+
+def traverseGraph(graph: Dict[str, Node]) -> List[Tuple[str, str]]:
+    pass
 
 def main():
     parser=argparse.ArgumentParser(description="test script to find longest in-key song mix")
@@ -14,8 +19,18 @@ def main():
 
     graph_builder =SongKeyGraphBuilder(args.song_dir)
     graph = graph_builder.getGraph()
-
-    print(graph.items())
+    mixed_songs = traverseGraph(graph)
+    
+    print(
+        f"""
+        --------------------------------------------------------------------
+        The longest order of songs that can be mixed in-key from the options  
+        provided in {args.song_dir} is the following:
+        --------------------------------------------------------------------\n
+        """
+    )
+    for song in mixed_songs:
+        print(song[0], "->", song[1])
 
 if __name__ == "__main__":
     main()
