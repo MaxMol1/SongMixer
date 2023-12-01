d691f55b25070bac2e6c55f25bd9973925de09e6
MaxMol1 <mmolchanov@ucdavis.edu>
2023-11-30 18:31:06 -0800

    Generate song graph

--- main.py
+++ main.py
@@ -41,6 +41,14 @@ MAJOR_PROFILE = [6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.2
 MINOR_PROFILE = [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
 
 
+def get_adjacent_keys(key: str) -> List[str]:
+    maj, mi = int(key[:-1]), key[-1]
+    maj_down = str((maj-1)%12) if maj != 1 else "12"
+    maj_up = str((maj+1)%12) if maj != 11 else "12"
+    mi_change = 'B' if mi == 'A' else 'A'
+    return [key, str(maj)+mi_change, maj_down+mi, maj_up+mi]
+
+
 class SongKeyGraphBuilder():
     @dataclass
     class Node():
@@ -55,11 +63,11 @@ class SongKeyGraphBuilder():
     def __init__(self, song_dir: str) -> None:
         self.song_dir = song_dir
         self._read_song_keys()
-        # self._create_graph()
+        self._create_graph()
 
-    def _read_song_keys(self):
+    def _read_song_keys(self) -> None:
         for filename in os.listdir(self.song_dir):
-            song_name = filename[:-4]  # TODO: strip the filename better
+            song_name = filename[:-4]  # TODO: strip the filename better (can't assume .mp3 extension e.g. ds_store)
             song_path = os.path.join(self.song_dir, filename)
 
             # load file into chromograph
@@ -84,24 +92,30 @@ class SongKeyGraphBuilder():
             # this attribute represents the key determined by the algorithm
             key = max(key_dict, key=key_dict.get)
             alpha_num_key = KEY_TO_ALPHANUMERIC[key]
-            # bestcorr = max(key_dict.values()) # NOTE: this is for debugging
+
+            # NOTE: this is for debugging
+            # bestcorr = max(key_dict.values())
 
             self.song_name_to_song_key[song_name] = alpha_num_key
             self.song_key_to_song_names[alpha_num_key].append(song_name)
 
-    def _create_graph(self):
+    def _create_graph(self) -> None:
         for song_name, song_key in self.song_name_to_song_key.items():
-            song_node = self.Node(
+            self.graph[song_name] = self.Node(
                 song_name=song_name,
                 song_key=song_key,
+                songs_in_key=[],
             )
-            # TODO: generate this, how to deal with wrapparound?
-            adj_keys = [-1, +1, +0, -0]
-            songs_in_key = [s for k in adj_keys for s in self.key_to_songs[k]]
+        self._create_in_key_connections()
+
+    def _create_in_key_connections(self) -> None:
+        for song_name, song_key in self.song_name_to_song_key.items():
+            songs_in_key = [s for k in get_adjacent_keys(song_key) for s in self.song_key_to_song_names[k]]
+            songs_in_key.remove(song_name)
             for song in songs_in_key:
-                self.graph[song].songs_in_key += song
-            song_node.songs_in_key = songs_in_key
-            self.graph[song_name] = song_node
+                if song_name not in self.graph[song].songs_in_key:
+                    self.graph[song].songs_in_key.extend(song_name)
+            self.graph[song_name].songs_in_key = songs_in_key
 
 
 def main():
@@ -114,7 +128,6 @@ def main():
     args=parser.parse_args()
 
     graph = SongKeyGraphBuilder(args.song_dir)
-    # TODO: find path
 
 if __name__ == "__main__":
     main()
